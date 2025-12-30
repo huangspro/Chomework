c98ae231cafb549f634a50dd29c129e807f1978e
hhy <13612276628@163.com>
2025-12-30 21:56:49 +0800

    1.0

--- Cruiser.C
+++ Cruiser.C
@@ -19,23 +19,23 @@ void Cruiser::fire(){
 
 void Cruiser::fire(){
   int d[]={1,2,3,4};
+  if(bullet<=0 && missile<=0)remove();
   switch(rand()%2){
     case 0:
       if(bullet<=0)return;
       game->addWeapons(row, col, Bullet_n,d[rand()%4], 1);
       bullet--;break;
     case 1:
-      if(bullet<=0)return;
+      if(missile<=0)return;
       game->addWeapons(row, col, Missile_n,d[rand()%4], 1);
       bullet--;break;
   }
 }
 
 void Cruiser::update(){
-  
   Ship::update();
   move(direction, 1);
-  if(rand()%10<3)fire();
+  if(rand()%10<1)fire();
 }
 
 void Cruiser::remove(){
