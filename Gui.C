3af0fa683a128df5f9fc46dfd6eae323609c2b03
hhy <13612276628@163.come.c.com>
2026-01-03 20:32:24 +0800

    4.0

--- Gui.C
+++ Gui.C
@@ -18,6 +18,8 @@ void Gui::init() {
     start_color();
     init_pair(1,COLOR_GREEN,COLOR_BLACK);
     init_pair(2,COLOR_RED,COLOR_BLACK);
+    init_pair(3,COLOR_WHITE,COLOR_WHITE);
+    bkgd(COLOR_PAIR(3));
 }
 
 int Gui::get() {
